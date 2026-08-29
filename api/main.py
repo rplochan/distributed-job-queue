db833d76e7555bf40cc51063d2918dfbc6b66459
rplochan <puttaiahr56@gmail.com>
2026-08-29 19:38:33 +0530

    api layer added and /job_status endpoint added

--- api/main.py
+++ api/main.py
@@ -22,12 +22,43 @@ def root():
 
 @app.post("/jobs")
 def create_job(job : JobRequest):
+    # generate a unique id
     job_id = redis_client.incr("job_id")
+
+    #convert job into the format..
     job_data = f"{job_id}|{job.type}|{job.a}|{job.b}"
+
+    # store the initial job status
+    redis_client.set(
+        f"job: {job_id}:status",
+        "queued"
+    )
+
+    #add it to the queue
     redis_client.lpush("jobs", job_data)
 
     return {
         "job_id": job_id,
         "status": "queued"
     }
+
+
+
+@app.get("/jobs/{job_id}")
+def get_job_status(job_id : int):
+
+    status = redis_client.get(
+        f"job:{job_id}:status"
+    )
+
+    if status is None:
+        return {
+            "error": "Job not found"
+        }
+
+    return {
+        "job_id": job_id,
+        "status": status
+    }
+
 
